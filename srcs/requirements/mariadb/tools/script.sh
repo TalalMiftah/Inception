dc7636f8001a5afb524f3d460cbc170f650c5909
Talal Miftah <talalmiftahvrry@gmail.com>
2024-03-13 14:51:18 +0000

    done in the god willing

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Start the MariaDB service
-service mariadb start
+mysqld_safe &
 
-# Wait for MariaDB to start
-while ! mysqladmin ping -hlocalhost --silent; do
-    sleep 1
-done
+sleep 10
 
-# Run SQL command to create database
-mysql -uroot -e"CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+sed -i 's/bind-address            = 127.0.0.1/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-# Keep the script running to keep the container alive
-tail -f /dev/null
+mariadb -u root -e"CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mariadb -u root -e"CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
+mariadb -u root -e"GRANT ALL PRIVILEGES ON $MYSQL_DATABASE .* TO '$MYSQL_USER'@'%';"
+mariadb -u root -e"FLUSH PRIVILEGES;"
+mariadb -u root -p${MYSQL_ROOT_PASSWORD} -e"ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mariadb -u root -p${MYSQL_ROOT_PASSWORD} -e"shutdown"
+
+exec mysqld
---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sleep 20
+
+wp core download --allow-root --path=/var/www/html
+
+wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_USER_PASSWORD --dbhost=mariadb:3306 --path=/var/www/html
+
+wp core install --allow-root --url=$DOMAIN_NAME --title=$TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --path=/var/www/html
+
+wp user create $WP_NORMAL_USER $WP_NORMAL_EMAIL --allow-root --role=subscriber --user_pass=$WP_NORMAL_PASSWORD --path=/var/www/html
+
+sed -i 's#listen = /run/php/php7.4-fpm.sock#listen = 0.0.0.0:9000#' /etc/php/7.4/fpm/pool.d/www.conf
+
+exec /usr/sbin/php-fpm7.4 -F
